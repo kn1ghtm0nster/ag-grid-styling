3e79bcbd9cf1ee1c426753cad1ca190fbaa106b0
Diego Quintanilla <diego.quintanilla@proton.me>
2023-03-29 22:37:12 -0400

    add styles directory and starting code for ag-grid styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatButtonModule } from '@angular/material/button';
+import { HttpClientModule } from '@angular/common/http'
+import { AgGridModule } from 'ag-grid-angular';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatButtonModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MatButtonModule, AgGridModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
